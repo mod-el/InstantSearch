819387eec8ce6d06b4223a83b9c5408b6fa9297d
Domenico <sviluppo@editt.it>
2017-10-13 17:04:54 +0200

    Auo loading of Popup and ContextMenu modules

--- InstantSearch.php
+++ InstantSearch.php
@@ -3,6 +3,20 @@ namespace Model;
 
 class InstantSearch extends Module{
 	/**
+	 * Takes advantage of the Popup and ContextMenu modules, if present
+	 *
+	 * @param mixed $options
+	 */
+	function init($options){
+		if($this->model->moduleExists('ContextMenu') and $this->model->moduleExists('Popup')){
+			if(!$this->model->isLoaded('ContextMenu'))
+				$this->model->load('ContextMenu');
+			if(!$this->model->isLoaded('Popup'))
+				$this->model->load('Popup');
+		}
+	}
+
+	/**
 	 * Controller for API actions
 	 *
 	 * @param array $request
