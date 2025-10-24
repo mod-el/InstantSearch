492c6544da8db3cd7709d59398510f9872383026
Domenico <zorinik@gmail.com>
2025-10-24 21:47:48 +0200

    tolgo slash

--- Providers/RouterProvider.php
+++ Providers/RouterProvider.php
@@ -8,7 +8,7 @@ class RouterProvider extends AbstractRouterProvider
 	{
 		return [
 			[
-				'pattern' => '/instant-search',
+				'pattern' => 'instant-search',
 				'controller' => 'InstantSearch',
 			],
 		];
